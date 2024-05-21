c99c875ddd9e9e33d001b0a80f1a93d8862a01c8
abusenet <88513508+abusenet@users.noreply.github.com>
2024-05-21 11:01:33 -0500

    fix: cors with middlewares

---
+++ functions/_middleware.js
@@ -0,0 +1,40 @@
+async function errorHandling(context) {
+  try {
+    return await context.next();
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
+}
+
+async function cors(context) {
+  const { request } = context;
+
+  const origin = request.headers.get("Origin") || "*";
+  const allowMethods = "HEAD, GET, POST";
+
+  if (request.method == "OPTIONS") {
+    const resp = new Response(null, {
+      status: 204,
+    });
+    const headers = resp.headers;
+    headers.set("Access-Control-Allow-Origin", origin);
+    headers.set("Access-Control-Allow-Methods", allowMethods);
+    headers.set("Access-Control-Allow-Private-Network", "true");
+    headers.set("Access-Control-Allow-Credentials", "true");
+    return resp;
+  }
+  const response = await context.next();
+  const headers = response.headers;
+
+  headers.set("Access-Control-Allow-Origin", origin);
+  headers.set("Access-Control-Allow-Credentials", "true");
+  headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+  );
+  headers.set("Access-Control-Allow-Methods", allowMethods);
+
+  return response;
+}
+
+export const onRequest = [errorHandling, cors];
